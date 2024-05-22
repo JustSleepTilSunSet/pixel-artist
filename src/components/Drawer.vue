7480432e8c797032756a3cc1bd91294c67cb7cae
a3770008 <a3770008@yahoo.com.tw>
2024-05-22 20:11:59 +0800

    Feat,complete the pixel canvas.

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,93 +1,103 @@
 <template>
   <div>
-    <canvas id="canvas" @mousedown="toDrawMode()" @mousemove="toDraw($event)" @mouseup="toCancelDraw()">
-      An alternative text describing what your canvas displays.
-    </canvas>
-    <button id="btn1" @click="toDownloadPainting()">
-      download
-    </button>
+    <canvas
+      ref="canvas"
+      width="300"
+      height="300"
+      @mousemove="color"
+    ></canvas>
   </div>
+
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import { ref } from 'vue';
 
-@Options({
-  props: {
-  },
-  data() {
+import { defineComponent, ref, onMounted } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const canvas = ref<HTMLCanvasElement | null>(null);
     const drag = ref(false);
-    return {
-      drag
-    }
-  },
-  mounted() {
-    const canvas = document!.querySelector("canvas");
-    const ctx = canvas!.getContext("2d");
-    // Set b.g. color.
-    ctx!.fillStyle = "#FFFFFF";
-    ctx!.fillRect(0, 0, canvas!.width, canvas!.height);
+    const squares = ref(
+      Array.from({ length: 30 }, () =>
+        Array.from({ length: 30 }, () => ({ selected: false }))
+      )
+    );
 
-    // Draw grid.
-    ctx!.beginPath();
-    for (let xOffset = 20; xOffset < canvas!.width; xOffset += 20) {
-      ctx!.moveTo(xOffset, 0);
-      ctx!.lineTo(xOffset, canvas!.height);
-    }
-    for (let yOffset = 20; yOffset < canvas!.height; yOffset += 20) {
-      ctx!.moveTo(0, yOffset);
-      ctx!.lineTo(canvas!.width, yOffset);
-    }
-    ctx!.stroke(); // Render the path
-  },
-  methods: {
-    toDrawMode: function () {
-      this.drag = true;
-    },
+    const draw = () => {
+      const ctx = canvas.value?.getContext('2d');
+      if (ctx) {
+        ctx.fillStyle = '#FFFFFF';
+        ctx.fillRect(0, 0, 300, 300);
 
-    toDraw: function (event: any) {
-      const canvas = document!.querySelector("canvas");
-      const ctx = canvas!.getContext("2d");
-      var dragEnd: any;
-      if (this.drag) {
-        let rect = canvas!.getBoundingClientRect();
-        dragEnd = {
-          x: event.clientX - rect!.left,
-          y: event.clientY - rect!.top
+        // 繪製網格
+        ctx.beginPath();
+        for (let xOffset = 10; xOffset < 300; xOffset += 10) {
+          ctx.moveTo(xOffset, 0);
+          ctx.lineTo(xOffset, 300);
         }
-        // TODO: to ambiguous coordinate.
-        ctx!.fillStyle = "green";
-        ctx!.fillRect(dragEnd.x, dragEnd.y, 5, 5);
+        for (let yOffset = 10; yOffset < 300; yOffset += 10) {
+          ctx.moveTo(0, yOffset);
+          ctx.lineTo(300, yOffset);
+        }
+        ctx.stroke();
+
+        // 繪製方格
+        squares.value.forEach((line, i) => {
+          line.forEach((square, j) => {
+            ctx.fillStyle = square.selected ? 'red' : 'white';
+            ctx.fillRect(10 * j + 1, 10 * i + 1, 9, 9);
+          });
+        });
       }
-    },
+    };
 
-    toCancelDraw: function () {
-      this.drag = false;
-    },
+    const color = (event: MouseEvent) => {
+      const rect = canvas.value!.getBoundingClientRect();
+      const x = Math.floor((event.clientX - rect.left) / 10);
+      const y = Math.floor((event.clientY - rect.top) / 10);
+      if (x >= 0 && x < 30 && y >= 0 && y < 30) {
+        squares.value[y][x].selected = true;
+        draw();
+      }
+    };
 
-    toDownloadPainting: function () {
-      const canvas = document!.querySelector("canvas");
-      var a = document.createElement("a");
-      a.href = "data:image/png;base64," + canvas?.toDataURL("image/jpeg").split(';base64,')[1]; //Image Base64 Goes here
-      a.download = "Image.jpg";
-      a.click();
+    const toDrawMode = (isDrawing: boolean) => {
+      drag.value = isDrawing;
+    };
 
-      // TODO:submit the base64 buffer to backend.
-      // console.log(canvas?.toDataURL("image/jpeg").split(';base64,')[1]);
-      // canvas?.toDataURL("image/jpeg")
-    }
+    const toDraw = (event: MouseEvent) => {
+      if (drag.value) {
+        const ctx = canvas.value?.getContext('2d');
+        if (ctx) {
+          const rect = canvas.value!.getBoundingClientRect();
+          const x = event.clientX - rect.left;
+          const y = event.clientY - rect.top;
+          ctx.fillStyle = 'green';
+          ctx.fillRect(x, y, 30, 30);
+        }
+      }
+    };
+
+    onMounted(() => {
+      draw();
+    });
+
+    return {
+      canvas,
+      drag,
+      toDrawMode,
+      toDraw,
+      color
+    };
   }
-})
-export default class Drawer extends Vue {
-}
+});
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 canvas {
   border: 1px solid;
-  width: 120;
-  height: 120;
 }
-</style>
+</style> 
