d4db7ec1d7e119f4c5fa700a8af38ef97c256017
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-05-28 00:24:03 +0800

    Test deploy.

--- src/services/pixelServerCli.ts
+++ src/services/pixelServerCli.ts
@@ -1,19 +1,19 @@
-import axios from 'axios';
-const uploadImageUrl = "http://127.0.0.1:10000/saveImage";
+// import axios from 'axios';
+// const uploadImageUrl = "http://127.0.0.1:10000/saveImage";
 
   
-export default {
-    async saveImage(image:String){
-        const data = {
-            image: image
-        };
+// export default {
+//     async saveImage(image:String){
+//         const data = {
+//             image: image
+//         };
 
-        axios.post(uploadImageUrl, data)
-            .then(response => {
-                console.log('Response:', response.data);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    }
-}
+//         axios.post(uploadImageUrl, data)
+//             .then(response => {
+//                 console.log('Response:', response.data);
+//             })
+//             .catch(error => {
+//                 console.error('Error:', error);
+//             });
+//     }
+// }
